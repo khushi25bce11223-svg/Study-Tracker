21d5b59122d455280c35b2ff3346aaf4e1542f94
khushi25bce11223-svg <khushi.25bce11223@vitbhopal.ac.in>
2025-11-23 00:43:39 +0530

    Create Core_Functionalities.py

---
+++ Core_Functionalities.py
@@ -0,0 +1,40 @@
+# Add a new subject
+def add_subject(data):
+    subject = input("Enter subject name: ")
+    if subject in data:
+      print("Subject already exists!")
+    else:
+      data[subject] = 0
+      print("Subject added.")
+# Update study hours
+def update_hours(data):
+    subject = input("Enter subject name: ")
+    if subject in data:
+      try:
+        hours = float(input("Enter hours to add: "))
+        data[subjects] += hours
+        print("Hours updated.")
+      except:
+        print("Invalid input ! Enter a number: ")
+      else:
+        print("Subject not found.")
+
+# View study report
+def view_report(data):
+   if not data:
+      print("No Subjects added yet.")
+   else:
+     print("\n--- Study Report ---")
+     for subject, hours in data.items():
+               print(f"{subject}: {hours} hours")
+       print()
+# Delete subject
+def delete_subject(data):
+    subject = input("Enter subject to delete: ")
+    if subject in data:
+       del data[subject]
+       print("Subject deleted.")
+    else:
+       print("Subject not found.")
+        
+        
